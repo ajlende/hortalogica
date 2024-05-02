00bcb14ca30bc9fe06c1133f1cf969f3d762c67b
nick <nmont16@gmail.com>
2024-05-01 20:01:27 -0500

    updating dependencies

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,13 @@ use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
 use esp_backtrace as _;
 use esp_hal::{
-    adc::{AdcConfig, Attenuation, ADC},
+    analog::adc::{AdcConfig, Attenuation, ADC},
     clock::ClockControl,
-    embassy::{self},
+    embassy,
     gpio::IO,
     peripherals::{Peripherals, ADC1},
     prelude::*,
-    timer::TimerGroup,
+    timer::{TimerGroup, TimerInterrupts},
 };
 use esp_println::println;
 
@@ -23,7 +23,8 @@ async fn main(_spawner: Spawner) {
     let system = peripherals.SYSTEM.split();
     let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
 
-    let timg0 = TimerGroup::new(peripherals.TIMG0, &clocks);
+    // let timerInterrupt: Option<TimerInterrupts> = TimerInterrupts.``
+    let timg0 = TimerGroup::new_async(peripherals.TIMG0, &clocks);
     embassy::init(&clocks, timg0);
 
     let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
@@ -39,11 +40,11 @@ async fn main(_spawner: Spawner) {
 
     // Main loop.
     loop {
-        power_pin.set_high().unwrap();
+        power_pin.set_high();
         Timer::after(Duration::from_millis(10_000)).await;
-        let pin_value: u16 = nb::block!(adc1.read(&mut moisture_pin)).unwrap();
+        let pin_value: u16 = nb::block!(adc1.read_oneshot(&mut moisture_pin)).unwrap();
         println!("Moisture = {}", pin_value);
-        power_pin.set_low().unwrap();
+        power_pin.set_low();
         Timer::after(Duration::from_millis(5_000)).await;
     }
 }
